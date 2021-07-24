9f68225667ae2a5061a35966a512502549750e95
Anique <aniquetahir@pm.me>
2021-07-23 23:11:25 -0700

    Added pyspark for faster processing

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from tqdm import tqdm
 import networkx as nx
 # import node2vec.src.node2vec as node2vec
 from node2vec import Node2Vec
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
 
 from gensim.models import Word2Vec
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+sc: SparkContext = SparkContext.getOrCreate(SparkConf().setMaster('local[*]'))
+ss: SparkSession = SparkSession.builder.getOrCreate()
 
 N2V_P = 1
 N2V_Q = 1
@@ -20,7 +25,7 @@ N2V_WINDOW_SIZE = 10
 N2V_ITER = 1
 N2V_WORKERS = 4
 NODE_LIMIT = -1
-COMMUNITY_LIMIT = 100
+COMMUNITY_LIMIT = 500
 
 
 def print_hi(name):
@@ -31,6 +36,17 @@ def read_ungraph(filepath: str):
     table = pd.read_csv(filepath, sep='\t', comment='#', names=['from', 'to'])
     return table
 
+def filter_edges(edges, include_list):
+    print('Creating edges RDD... ')
+    rdd_edges = ss.createDataFrame(edges).rdd.map(list)
+    print('Spark filtering... ')
+    rdd_edges = rdd_edges.filter(lambda x: x[0] in include_list and x[1] in include_list)
+    filtered_edges = rdd_edges.collect()
+    print(f'Filtered to {len(filtered_edges)} edges')
+    # filtered_edges = [(i, j) for i, j in tqdm(edges) if i in include_list and j in include_list]
+    return filtered_edges
+
+
 def get_top5000_nx_graph(graph_path, community_path):
     cmts = read_communities(community_path)
     cmts = cmts[:COMMUNITY_LIMIT]
@@ -38,7 +54,10 @@ def get_top5000_nx_graph(graph_path, community_path):
     cty_nodes = list(cty_nodes)[:NODE_LIMIT]
     G = nx.Graph()
     edges = read_ungraph(graph_path)
-    G.add_edges_from(edges.to_numpy())
+    edges = filter_edges(edges, cty_nodes)
+    print('Adding edges to nx graph...')
+    G.add_edges_from(edges)
+    print('Removing extra nodes...')
     removal_nodes = [n for n in G.nodes if n not in cty_nodes]
     G.remove_nodes_from(removal_nodes)
     return G
@@ -79,9 +98,10 @@ def create_n2v_embeddings(graph_location, save_path, community_path):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    create_n2v_embeddings('datasets/livejournal/com-lj.ungraph.txt', 'lj.n2v.emb', 'datasets/livejournal/com-lj.top5000.cmty.txt')
-    create_n2v_embeddings('datasets/orkut/com-orkut.ungraph.txt', 'orkut.n2v.emb', 'datasets/orkut/com-orkut.top5000.cmty.txt')
-    create_n2v_embeddings('datasets/friendster/com-friendster.ungraph.txt', 'friendster.n2v.emb', 'datasets/friendster/com-friendster.top5000.cmty.txt')
+    print('Starting...')
+    create_n2v_embeddings('../../datasets/livejournal/com-lj.ungraph.txt', 'lj.n2v.emb', '../../datasets/livejournal/com-lj.top5000.cmty.txt')
+    create_n2v_embeddings('../../datasets/orkut/com-orkut.ungraph.txt', 'orkut.n2v.emb', '../../datasets/orkut/com-orkut.top5000.cmty.txt')
+    create_n2v_embeddings('../../datasets/friendster/com-friendster.ungraph.txt', 'friendster.n2v.emb', '../../datasets/friendster/com-friendster.top5000.cmty.txt')
     # communities = read_communities('datasets/livejournal/com-lj.top5000.cmty.txt')
     # edges = read_ungraph('datasets/livejournal/com-lj.ungraph.txt')
     # G = nx.Graph()
