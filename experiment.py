835655f9e86a63c4916ecbfe440346c4a0bed085
Anique <aniquetahir@pm.me>
2021-08-03 18:11:28 -0700

    Adding generic embedding functions

--- experiment.py
+++ experiment.py
@@ -1,1 +1,79 @@
-from sklearn.linear_model import LogisaticRegression
+import stellargraph as sg
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import os
+import networkx as nx
+import numpy as np
+import pandas as pd
+from stellargraph import datasets
+import stellargraph as sg
+from stellargraph.data import BiasedRandomWalk
+from IPython.display import display, HTML
+from scipy.io import loadmat
+import ge
+
+import pickle
+from gensim.models import Word2Vec
+
+
+def n2v_embeddings(path_to_mat, path_to_walks, emb_path, emb_dim=128):
+    bc_mat = loadmat(path_to_mat)
+    bc_G = nx.from_scipy_sparse_matrix(bc_mat['network'])
+    sg_bc_G = sg.StellarGraph.from_networkx(bc_G)
+    print(sg_bc_G.info())
+    print('='*10)
+    rw = BiasedRandomWalk(sg_bc_G)
+
+    if os.path.exists(path_to_walks):
+        with open(path_to_walks, 'rb') as walks_file:
+            walks = pickle.load(walks_file)
+    else:
+        walks = rw.run(
+            nodes=list(sg_bc_G.nodes()),  # root nodes
+            length=100,  # maximum length of a random walk
+            n=10,  # number of random walks per root node
+            p=0.5,  # Defines (unormalised) probability, 1/p, of returning to source node
+            q=2.0,  # Defines (unormalised) probability, 1/q, for moving away from source node
+        )
+        print("Number of random walks: {}".format(len(walks)))
+        with open(path_to_walks, 'wb') as walk_file:
+            pickle.dump(walks, walk_file)
+    print("Done walking.")
+    str_walks = [[str(n) for n in walk] for walk in walks]
+    full_emb_path = f'{emb_path}_{emb_dim}'
+    if not os.path.exists(full_emb_path):
+        model = Word2Vec(str_walks, vector_size=emb_dim, window=5, min_count=0, sg=1, workers=2, epochs=1)
+        model.wv.save_word2vec_format(full_emb_path)
+
+
+def generic_embeddings(embedding_function, function_params, train_params, path_to_mat, emb_path):
+    if not os.path.exists(emb_path):
+        mat = loadmat(path_to_mat)
+        G = nx.from_scipy_sparse_matrix(mat['network'])
+        function_params['G'] = G
+        model = embedding_function(**function_params)
+        model.train(**train_params)
+        embeddings = model.get_embeddings()
+        with open(emb_path, 'wb') as emb_file:
+            pickle.dump(emb_file)
+
+
+if __name__ == "__main__":
+    # print('=' * 20)
+    # print('Blog Catalog')
+    # n2v_embeddings('/dmml_pool/datasets/graph/blogcatalog.mat', 'sc_bc_walks.pkl', 'sc_bc.emb')
+    # print('=' * 20)
+    # print('Flickr')
+    # n2v_embeddings('/dmml_pool/datasets/graph/flickr.mat', 'sc_flickr_walks.pkl', 'sc_flickr.emb')
+    print('=' * 20)
+    print('YouTube')
+    n2v_embeddings('/dmml_pool/datasets/graph/youtube.mat', 'sc_youtube_walks.pkl', 'sc_flickr.emb')
+
+    # generic_embeddings()
+
+
+
+
+
+
