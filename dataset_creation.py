b9c84951b80da1d0c8eaa4b4e35ecac8fcd4e03b
Anique <aniquetahir@pm.me>
2021-08-28 16:25:15 -0700

    save generated data intermediately

--- dataset_creation.py
+++ dataset_creation.py
@@ -6,7 +6,11 @@ from anique import *
 if __name__ == "__main__":
     num_samples = 10000
     synthetic_data = []
-    for _ in tqdm(range(num_samples)):
+    for i in tqdm(range(num_samples)):
+        if i % 10 == 1:
+            save_pickle(synthetic_data, f'synth_cache/data.{i}.pkl')
+            del synthetic_data
+            synthetic_data = []
         G, labels = get_uniform_random_sbm(50, 200)
         # 2-d embeddings
         r_embeddings = get_reduced_community_embeddings_from_gt(G, labels)
@@ -14,6 +18,7 @@ if __name__ == "__main__":
         for perturb_intensity in range(0, 1000, 100):
             pert_embeddings, energy = perturb_embeddings(r_embeddings, perturb_intensity)
             synthetic_data.append((list(G.edges), pert_embeddings, energy,))
+        del G, labels
 
     # TODO save synthetic data
     save_pickle(synthetic_data, 'synthetic_energy_data.pkl')
